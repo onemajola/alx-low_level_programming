2939cdf9c46d86a8bc2c92185695e441816c2768
onemajola <majolabeat@gmail.com>
2023-02-18 05:03:49 +0200

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+* main - Print lower and uppercase alphabet
+* Return: Always (Success)
+*/
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+	
+	return (0);
+}
